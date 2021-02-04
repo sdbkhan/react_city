11e0e5eda1f146529af8c1e67b3fc55f3af846f4
unknown <admin@CHETUIWK413.chetu.com>
2021-02-04 16:32:31 +0530

    file update

---
+++ src/Sdata.jsx
@@ -0,0 +1,28 @@
+const Sdata=[
+    {
+        imgsrc:"https://media.easemytrip.com/media/Blog/India/636930188375829142/636930188375829142oUAXWh.jpg",
+title:"Most beutifull city",
+sname:"Mumbai, Maharashtra",
+link:"https://mumbaicity.gov.in/",
+},
+{
+ imgsrc:"https://media.easemytrip.com/media/Blog/India/636930188375829142/636930188375829142yg930l.jpg",
+title:"Most beutifull city",
+sname:"Pune, Maharashtra",
+link:"https://pune.gov.in/" ,
+},
+{
+ imgsrc:"https://media.easemytrip.com/media/Blog/India/636930188375829142/636930188375829142Dk6LTx.jpg",
+title:"Most beutifull city",
+sname:"Bangalore, Karnataka",
+link:"https://bengaluruurban.nic.in/en/",
+},
+{
+ imgsrc:"https://media.easemytrip.com/media/Blog/India/636930188375829142/636930188375829142rpoqWk.jpg",
+title:"Most beutifull city",
+sname:"Chennai, Tamil Nadu",
+link:"https://chennai.nic.in/",
+    },
+    
+];
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Card from "./Card"
+import Sdata from "./Sdata";
+
+// function ncard(val) {
+//   return(
+    
+// <Card
+// imgsrc={val.imgsrc}
+// title={val.title}
+// sname={val.sname}
+// link={val.link}
+// />
+//   )
+// }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+<>
+
+
+{Sdata.map((val, index)=>{
+  return (
+    
+<Card
+imgsrc={val.imgsrc}
+title={val.title}
+sname={val.sname}
+link={val.link}
+/>
+  );
+})}
+
+</>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
